a035fbf5a8d0e106bc64a80ad8664ae5c653e34e
theed <kareoedwin@gmail.com>
2023-08-02 16:34:09 +0300

    add route

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/soma/receiver', [\App\Http\Controllers\SomachatController::class, 'receiver'])->name('soma.receiver');
